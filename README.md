07a49bca1004fb3b4647278d7e6d5a7746151ba6
delgesu2 <xavierus70@hotmail.com>
2019-08-02 16:19:49 +0200

    Fini ! TOut est bon !

---
+++ README.md
@@ -0,0 +1,4 @@
+Bonjour ! 
+Il s'agit de réaliser une petite page web affichant une liste de liens.
+
+L'énoncé au complet est disponible sur le site [OpenClassrooms](http://exercices.openclassrooms.com/assessment/533?id=3306901&slug=creez-des-pages-web-interactives-avec-javascript&login=7872918&tk=e1046a52e6dd16d91490063ba646c37c&sbd=2016-02-01&sbdtk=2466d6bae51e373d89ac8e3f74213199)
--- js/liensweb.js
+++ js/liensweb.js
@@ -26,37 +26,51 @@ var listeLiens = [
 
 // TODO : compléter ce fichier pour ajouter les liens à la page web
 
-
 for (var lien of listeLiens) {
 
-    var contenu = document.getElementById('contenu');
+    const contenu = document.getElementById('contenu');
+
+    // Création des éléments HTML
     var title = document.createElement("span");
     var link = document.createElement("span");
     var author = document.createElement("p");
+    var bloc = document.createElement("div");
+
+    // Creation du text du titre
+    textLien = document.createTextNode(lien.titre);
+
+    // Création balise <a> + ajout du href
+    var a = document.createElement("a");
+    a.setAttribute("href", lien.url);
  
     // Rajout de la classe CSS .lien  
-    contenu.classList.add("lien");
+    bloc.classList.add("lien");
 
-    // Tout le style du titre
-    title.id = 'titre';    
+    // Tout le style du titre   
     title.style.color = "#428bca";
     title.style.fontSize = "1em";
     title.style.fontWeight = "bold"; 
-
-    // Contenu du titre
-    title.textContent = lien.titre;
    
     // Contenu du lien et de l'auteur
     link.textContent = lien.url;
-    author.textContent = lien.auteur;       
-      
-      //var blocTitre = getComputedStyle(document.getElementById("titre"));
-      //blocTitre.insertAdjacentHTML("beforebegin", "<a href='lien.url'>");
-    contenu.appendChild(title);
-      //blocTitre.insertAdjacentHTML("afterend", "</a");
-    
+    author.textContent = "Ajouté par " + lien.auteur; 
 
-    contenu.appendChild(link);
-    contenu.appendChild(author);
+    // On insert le lien
+    var lien = title.insertAdjacentElement('afterbegin', a);
+
+    // Style du lien
+    lien.style.textDecoration = "none";
+    lien.style.marginRight = "5px";
+       
+    // On met le texte du titre dans le lien
+    lien.appendChild(textLien);      
+  
+    // On met tous les éléments dans la <div> 'bloc'
+    bloc.appendChild(title);
+    bloc.appendChild(link);
+    bloc.appendChild(author);
+   
+    // Et les 'bloc' s'empilent dans chaque 'contenu' de la boucle for
+    contenu.appendChild(bloc);
 
 }
